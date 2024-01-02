3b67db36fd33afec0fcd4ad2445003d263a0561f
Gamer Snave <snaveqiptoo@gmail.com>
2024-01-02 20:54:22 +0300

    added date in timestamp

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const fs = require('fs');
 
 function getCurrentTime() {
   const now = new Date();
+  const year = now.getFullYear();
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const day = now.getDate().toString().padStart(2, '0');
   const hours = now.getHours().toString().padStart(2, '0');
   const minutes = now.getMinutes().toString().padStart(2, '0');
   const seconds = now.getSeconds().toString().padStart(2, '0');
-  return `${hours}:${minutes}:${seconds}`;
+  
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
+
+
+
+
 
 function generateRandomString(length) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
